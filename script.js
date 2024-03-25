b0747c3a31642098ce9a08ce49587398c9ad5fb5
meizu <rfsgalendez@gmail.com>
2024-03-26 04:28:25 +0800

    structure is done, needs polishing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
-
+//written by m3izu, looks janky but goodenough..... i think
+let noWin = 0;
+let noLoss = 0;
+let noTie = 0;
 
 function getComputerChoice() {
     let chosenNo = Math.floor(Math.random() * 3);
@@ -19,46 +21,54 @@ function getComputerChoice() {
 
 function playRound(playerSection, computerSelection) {
     
+    
+
     if (playerSection === 'ROCK') {
         if (computerSelection === 'PAPER') {
+            noLoss +=1;
             return 'PAPER w';
         }
         else if (computerSelection === 'SCISSOR') {
+            noWin += 1;
             return 'ROCK w';
         }
         else {
+            noTie += 1;
             return 'tie nigga';
         }
     }
     else if (playerSection === 'PAPER') {
         if (computerSelection === 'ROCK') {
+            noWin += 1;
             return 'PAPER w';
         }
         else if (computerSelection === 'SCISSOR') {
+            noLoss += 1;
             return 'SCISSOR w';
         }
         else if (computerSelection === 'PAPER') {
+            noTie += 1;
             return 'tie nigga'
         };
     }
     else if (playerSection === 'SCISSOR') {
         if (computerSelection === 'ROCK') {
+            noLoss += 1;
             return 'ROCK w';
         }
         else if (computerSelection === 'PAPER') {
+            noWin += 1;
             return 'SCISSOR w';
         }
         else if (computerSelection === 'SCISSOR') {
+            noTie += 1;
             return 'tie';
         }
     }
     else {
         return 'not an option'
     }
-
-
 }
-
 
 
 function playGame() {
@@ -69,5 +79,6 @@ function playGame() {
         let computerSelection = getComputerChoice();
         console.log(playRound(playerSection, computerSelection))
     }
+    console.log(`You have won ${noWin} time(s), lost ${noLoss} time(s), and have tied ${noTie} time(s)`)
 }
 playGame();
